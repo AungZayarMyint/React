dc532270c0fa003e4e8805078e59cfde5e1fe493
jonathan <aungzayar1352000@gmail.com>
2024-01-04 11:08:46 +0630

    error show

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,49 @@
-
-import useToggle from "./hooks/useToggle";
+import { useEffect, useState } from "react";
+import ProductCard from "./components/ProductCard";
 
 function App() {
-  const [isShow, toggle] = useToggle();
+  const [products, setProducts] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(false);
+
+  useEffect(() => {
+    getProducts();
+  },[]);
+
+  const getProducts = async() => {
+    try{
+    setLoading(true)
+    const response = await fetch("https://fakestoreapi.com/products");
+    if(!response.ok){
+      throw new Error("No products found, come back later.")
+    }
+    const products = await response.json()
+
+    setProducts(products);
+    }catch(error){
+      setError(error.message);
+    }
+    setLoading(false);
+  };
 
   return (
-    <section>
-      <button onClick={toggle}>
-        {isShow ? "hide" : "show"}</button>
+    <>
+      <h1 className="title">Store Universe</h1>
+      <section className="flex-ctr">
       {
-        isShow && <h1>component is showing</h1>
+        products.map((product) => (
+          <ProductCard key={product.id} product={product}/>
+        ))
       }
-    </section>
-  );
+      </section>
+      {
+        isLoading && <h1>loading products</h1>
+      }
+      {
+        isError && <h1>{isError}</h1>
+      }
+    </>
+  )
 }
 
 export default App;
